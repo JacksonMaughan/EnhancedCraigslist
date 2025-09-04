6b4be860bedc16466d26993eaa8f0ad9ad8e3d0b
Jackson Maughan <jacksonmaughan@Jacksons-MacBook-Air.local>
2025-09-04 09:58:11 -0700

    Added spring starter

---
+++ CLBackend/src/main/java/com/example/CLBackend/ClBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.CLBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClBackendApplication.class, args);
+	}
+
+}
---
+++ CLBackend/src/test/java/com/example/CLBackend/ClBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CLBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
